bb52a16767ac15c6dd49bfa88f333725023af477
Bande_Meliza_BSIT32E1 <148577459+AgonoyAlexisGreko-BSIT32E1@users.noreply.github.com>
2024-04-25 20:09:29 +0800

    Database connection

---
+++ Onion_Todolist_Unknownnn/Onion_Todolist_Unknownnn/Database.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+
+namespace Onion_Todolist_Unknownnn
+{
+    public class Database
+    {
+        private static string connectionString = @"";
+    }
+}
