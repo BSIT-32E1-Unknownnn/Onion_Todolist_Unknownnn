7b55a3305422c05748380b8ec55a4679756ab465
Bande_Meliza_BSIT32E1 <148577459+AgonoyAlexisGreko-BSIT32E1@users.noreply.github.com>
2024-04-25 20:21:32 +0800

    tables

---
+++ Onion_Todolist_Unknownnn/Onion_Todolist_Unknownnn/Models/Todolist.cs
@@ -0,0 +1,10 @@
+﻿namespace Onion_Todolist_Unknownnn.Models
+{
+    public class Todolist
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public string Date { get; set; }
+        public string Action { get; set; }
+    }
+}
