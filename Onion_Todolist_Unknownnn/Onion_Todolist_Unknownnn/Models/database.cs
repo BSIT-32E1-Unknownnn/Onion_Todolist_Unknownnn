8fe47152161c73fac7b6406992f77a0a3bd71910
BandeMeliza-BSIT32E1 <155070275+BandeMeliza-BSIT32E1@users.noreply.github.com>
2024-05-20 12:44:29 +0800

    update

--- Onion_Todolist_Unknownnn/Onion_Todolist_Unknownnn/Models/database.cs
+++ Onion_Todolist_Unknownnn/Onion_Todolist_Unknownnn/Models/database.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Onion_Todolist_Unknownnn.Models
 {
     public class Database : DbContext
     {
-        public DbSet<Todolist> TodoList {  get; set; }
+        public DbSet<Todolist> TodoList { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite(@"Data Source=C:\Users\USER\Documents\GitHub\Onion_Todolist_Unknownnn\Onion_Todolist_Unknownnn\Onion_Todolist_Unknownnn\Files\todolist.db");
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            options.UseSqlite(connectionString);
+        }
     }
 }
